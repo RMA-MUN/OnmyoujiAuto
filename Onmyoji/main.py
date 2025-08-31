7048b6c9702c3a915e5309bdd8791b548fed8a67
RMA-MUN <3032747608@qq.com>
2025-08-31 12:23:08 +0800

    Add files via upload

---
+++ Onmyoji/main.py
@@ -0,0 +1,15 @@
+"""
+@Author: RMA-MUN
+@email: n3032747608@163.com
+@Date: 2025/7/28/12:54
+@Version: 1.5.1
+@Location: https://gitee.com/rma-mun/onmyoji-automation
+"""
+
+# 打包： py2exe-gui
+
+import sys
+from core.app_launcher import launch_app
+
+if __name__ == "__main__":
+    sys.exit(launch_app())
