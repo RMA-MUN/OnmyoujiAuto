6acd5b873b2685b6c289a10ac63c1f81ce513220
RMA-MUN <3032747608@qq.com>
2025-08-31 12:40:40 +0800

    Add files via upload

---
+++ Onmyoji/config/package_config.py
@@ -0,0 +1,13 @@
+PACKAGE_CONFIG = {
+    "version": "1.3.1",
+    "author": "RMA-MUN",
+    "email": "n3032747608@163.com",
+    "icon_path": "icon.ico",
+    "build_command": [
+        "pyinstaller",
+        "--onefile",
+        "--windowed",
+        "--icon=icon.ico",
+        "main.py"
+    ]
+}
