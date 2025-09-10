5c38ba625baa112c23aa0363598489d656350ab9
RMA-MUN <3032747608@qq.com>
2025-09-10 16:19:41 +0800

    Update app_launcher.py

--- Onmyoji/core/app_launcher.py
+++ Onmyoji/core/app_launcher.py
@@ -1,4 +1,6 @@
 # 初始化应用程序
+import time
+
 from PyQt6 import QtWidgets
 
 from Onmyoji.utils.error_handler import handle_global_exception
@@ -11,8 +13,11 @@ def launch_app():
         window = MainWindow()
         window.show()
         print("程序启动成功")
-        print("新增稳定模式和快速模式。\n快速模式刷本更加迅速，但是安全性低，容易被检测封号\n"
-              "稳定模式刷本更加安全，但是速度较慢")
+        time.sleep(1)
+        # 新增稳定模式和快速模式
+        print("新增稳定模式和快速模式。\n"
+             "快速模式刷本更加迅速，但是安全性低，容易被检测封号\n"
+             "稳定模式刷本更加安全，但是速度较慢\n")
         return app.exec()
     except Exception as e:
         handle_global_exception(e, window)
