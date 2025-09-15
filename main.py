4ff97b437df0acd7b1ca0e423100314bc207de68
RMA-MUN <3032747608@qq.com>
2025-09-15 14:57:10 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+@Author: RMA-MUN
+@email: n3032747608@163.com
+@Date: 2025/9/06
+@Version: 1.5.2
+@Location: https://github.com/RMA-MUN/OnmyoujiAuto
+"""
+
+# 打包： py2exe-gui
+
+import sys
+from Onmyoji.core.app_launcher import launch_app
+
+if __name__ == "__main__":
+    sys.exit(launch_app())
