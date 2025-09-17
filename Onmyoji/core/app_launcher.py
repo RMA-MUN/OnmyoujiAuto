24eaaa78ad019131f4ccddd8fd9c55ce0f16a642
RMA-MUN <3032747608@qq.com>
2025-09-17 19:28:36 +0800

    Update app_launcher.py

--- Onmyoji/core/app_launcher.py
+++ Onmyoji/core/app_launcher.py
@@ -14,10 +14,6 @@ def launch_app():
         window.show()
         print("程序启动成功")
         time.sleep(1)
-        # 新增稳定模式和快速模式
-        print("新增稳定模式和快速模式。\n"
-             "快速模式刷本更加迅速，但是安全性低，容易被检测封号\n"
-             "稳定模式刷本更加安全，但是速度较慢\n")
         return app.exec()
     except Exception as e:
         handle_global_exception(e, window)
